b040f44f3f075aeac39f340be33938f7867b5faf
NoLevelCap - Mac <Capitals@Jamess-MacBook-Air.local>
2018-10-10 11:32:08 +0100

    InitialUpload

---
+++ Assignment1/src/net/robotics/main/MoveByColor.java
@@ -0,0 +1,17 @@
+package net.robotics.main;
+
+public class MoveByColor {
+	
+	private static RGBFloat GREEN = new RGBFloat(new RGBFloat(0f, 0.19f, 0f), new RGBFloat(0.15f, 0.25f, 0.15f));
+	private static RGBFloat WHITE = new RGBFloat(new RGBFloat(0.22f, 0.22f, 0.22f), new RGBFloat(0.3f, 0.3f, 0.3f));
+	private static RGBFloat BLACK = new RGBFloat(new RGBFloat(0.00f, 0.00f, 0.00f), new RGBFloat(0.05f, 0.05f, 0.05f));
+	private static RGBFloat BLUE = new RGBFloat(new RGBFloat(0.00f, 0.00f, 0.05f), new RGBFloat(0.075f, 0.055f, 0.1f));
+	private static RGBFloat YELLOW = new RGBFloat(new RGBFloat(0.25f, 0.15f, 0.00f), new RGBFloat(0.30f, 0.20f, 0.1f));
+	private static RGBFloat RED = new RGBFloat(new RGBFloat(0.20f, 0.00f, 0.00f), new RGBFloat(0.25f, 0.05f, 0.075f));
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
