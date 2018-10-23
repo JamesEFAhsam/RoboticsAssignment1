7e8ab78b1e2401e3b4475d71b7fc0c64799dd165
aslammuftah <44114025+aslammuftah@users.noreply.github.com>
2018-10-23 13:22:11 +0100

    updated the tiles class with new methods
    added methods to create nodes from the tiles to be used in the A* star algorithm

--- Assignment1/src/net/robotics/map/Tile.java
+++ Assignment1/src/net/robotics/map/Tile.java
@@ -1,9 +1,19 @@
 package net.robotics.map;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Tile {
 	private float occupiedBelief;
 	private int visited;
 	private int x, y;
+	//NEW FIELDS
+	private float g_score = 0;
+	private float h_score = 0;
+	private Tile cameFrom = null;
+	private ArrayList <Tile> tilesArray = new ArrayList<Tile>();
+	static final int FACTOR = 10;
+	//
 	
 	private int empty, viewed;
 	
@@ -58,4 +68,67 @@ public class Tile {
 	private void setY(int y) {
 		this.y = y;
 	}
+	
+	//NEW METHODS
+	protected Collection <Tile> getTiles() {
+		return tilesArray;
+	}
+	
+	protected int numberofTiles() {
+		return tilesArray.size();
+	}
+	
+	protected boolean addNewTile(Tile nextTile) {
+		if(tilesArray.contains(nextTile)) {
+			return false;
+		}
+		if(nextTile == this) {
+			return false;
+		}
+		tilesArray.add(nextTile);
+		return true;
+	}
+	
+	protected boolean removeTile(Tile nextTile) {
+		return tilesArray.remove(nextTile);
+	}
+	
+	protected void setH_Score(float h) {
+		h_score = h;
+	}
+	
+	protected double calculateG(Tile nextTile) {
+		double xDistance = this.x - nextTile.x;
+		double yDistance = this.y - nextTile.y;
+		double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance));		
+		return distance;
+		
+	}
+	
+	protected double calculateH(Tile targetTile) {
+		return calculateG(targetTile);
+	}
+	
+	protected void setG_Score(float g) {
+		g_score = g;
+	}
+	
+	protected float getG_Score(){
+		return g_score;
+	}
+	
+	protected float getF_Score(){
+		return g_score + h_score;
+	}
+	
+	protected Tile getPredecessor() {
+		return cameFrom;
+	}
+	
+	protected void setPredecessor(Tile origin) {
+		cameFrom = origin;
+	}
+	//
+	
+	
 }
